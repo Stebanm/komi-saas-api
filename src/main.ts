464f3bc8fb64a98f5b139716537c8b86f70f13a2
Stebanm <juanesxde16@gmail.com>
2026-06-19 17:24:31 -0500

    Se implementa nueva dependencia y se agrega validacion de la variable de entorno del puerto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') ?? 3000;
+
+  await app.listen(port);
+
+  logger.log(`🚀 Servidor corriendo exitosamente en: ${await app.getUrl()}`)
+};
+
 bootstrap();
