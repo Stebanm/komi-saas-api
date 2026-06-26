00e4190ea874a2824d156f450d1576f36f9b21de
Stebanm <juanesxde16@gmail.com>
2026-06-25 23:00:42 -0500

    Feature: Se implementa logica de dominio para los lotes de inventarios perecederos y no perecederos.

---
+++ src/context/inventory/batchs/domain/index.ts
@@ -0,0 +1,7 @@
+
+export * from './value-object';
+export * from './types';
+
+export * from './inventory-batch-created.event';
+export * from './inventory-batch.aggregate';
+export * from './inventory-batch.repository';
